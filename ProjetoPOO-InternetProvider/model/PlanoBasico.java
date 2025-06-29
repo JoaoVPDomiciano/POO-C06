d01efa9a80a37cc57c3133d45bc3568b4cc9acf6
JoaoVPDomiciano <joaovpdomiciano@gmail.com>
2025-06-29 12:23:39 -0300

    Delete ProjetoPOO-InternetProvider directory

--- ProjetoPOO-InternetProvider/model/PlanoBasico.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class PlanoBasico extends PlanoInternet {
-    public PlanoBasico(String nome, double preco, double velocidade){
-        super(nome,preco,velocidade);
-    }
-}
--- ProjetoPOO-InternetProvider/model/PlanoPremium.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class PlanoPremium extends PlanoInternet {
-    public PlanoPremium(String nome, double preco, double velocidade){
-        super(nome,preco,velocidade);
-    }
-}
